f5d43fa931ddedbc1ad8865c0c86dbece3f3fe4f
Alexandra <zero.gravitation28@gmail.com>
2023-07-09 12:32:04 +0600

    Задание_03_ver01

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,14 @@
-# TODO здесь писать код
+def reverse_float(num):
+    digits = str(num).split('.')
+    reversed_digits = [''.join(reversed(part)) for part in digits]
+
+    return float( '.'.join(reversed_digits) )
+
+a = reverse_float(input("Введите первое число: "))
+b = reverse_float(input("Введите второе число: "))
+
+print('Первое число наоборот: ', a)
+print('Второе число наоборот: ', b)
+print('Сумма: ', a + b)
 
 
