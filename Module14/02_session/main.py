0bd0b95fa53c50418b6cb77c7cbdb620146e88ac
Alexandra <zero.gravitation28@gmail.com>
2023-07-09 12:26:00 +0600

    Задание_02_ver01

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+print("Введите первую точку")
+x01 = float(input('X: '))
+y01 = float(input('Y: '))
+
+print("\nВведите вторую точку")
+x02 = float(input('X: '))
+y02 = float(input('Y: '))
+
+x_difference = x01 - x02
+y_difference = y01 - y02
+
+if x_difference == 0:
+    k = 1
+
+else:
+    k = y_difference / x_difference
+b = y02 - k * x02
+
+print("Уравнение прямой, проходящей через эти точки:")
+print("y = ", k, " * x + ", b)
