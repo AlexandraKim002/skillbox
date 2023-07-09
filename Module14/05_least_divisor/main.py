d46c122105840e799e353a155964852bb0063c9c
Alexandra <zero.gravitation28@gmail.com>
2023-07-09 12:34:09 +0600

    Задания 1 -5

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+def minimal_delit(number):
+    for i in range(2, number + 1):
+        if number % i == 0:
+            return i
+
+number = int(input("Введите число: "))
+print("Наименьший делитель, отличный от единицы:", minimal_delit(number))
